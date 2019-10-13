1cabededce785ee01f089e4feaa95b72aa94852f
aicpp <ai.cpp@yandex.ru>
2019-10-13 19:41:07 +0300

    add keys to shrink branch name; refactoring;

---
+++ git_branch_name_creator/git_branch_name_creator/arg_parser.h
@@ -0,0 +1,67 @@
+﻿#pragma once
+#include <string>
+#include <map>
+#include <sstream>
+
+namespace arg_parser
+{
+
+using CmdArguments = std::map<std::wstring, std::wstring>;
+
+inline bool HasArgument(const CmdArguments& arguments, const std::wstring& arg)
+{
+    return arguments.count(arg) > 0;
+}
+
+inline std::wstring GetArgumentOrDefault(const CmdArguments& arguments, const std::wstring& arg)
+{
+    const auto itFind = arguments.find(arg);
+    if (itFind != arguments.end())
+    {
+        return itFind->second;
+    }
+    return {};
+}
+
+inline bool IsExplicitArgument(const std::wstring& argv)
+{
+    const std::wstring matchPrefix = L"--";
+    return argv.size() > matchPrefix.size() && argv.find_first_of(matchPrefix) == 0;
+}
+
+inline CmdArguments ExtractExplicitArguments(int argc, wchar_t* argv[], int& position)
+{
+    CmdArguments result;
+    for (position = 1; (position < argc) && IsExplicitArgument(argv[position]); ++position)
+    {
+        std::wstring arg(argv[position]);
+        std::wstring argValue;
+
+        // get value if exists
+        if ((position + 1) < argc)
+        {
+            std::wstring nextArg(argv[position + 1]);
+            if (!IsExplicitArgument(nextArg))
+            {
+                argValue = nextArg;
+                position++;
+            }
+        }
+
+        result[arg] = argValue;
+    }
+
+    return result;
+}
+
+inline std::wstring ExtractRestArguments(int argc, wchar_t* argv[], int sincePosition = 1)
+{
+    std::wstringstream result;
+    for (int position = sincePosition; position < argc; position++)
+    {
+        result << argv[position] << " ";
+    }
+    return result.str();
+}
+
+} // arg_parser
---
+++ git_branch_name_creator/git_branch_name_creator/main.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+#include <Windows.h>
+#include <Shlwapi.h>
+#pragma comment (lib, "Shlwapi.lib")
+
+#include "string_transform.h"
+#include "clipboard.h"
+#include "arg_parser.h"
+
+
+namespace constants
+{
+const wchar_t* CommandArgFromClip     = L"--from_clip";
+const wchar_t* CommandArgToClip       = L"--to_clip";
+const wchar_t* CommandArgBranchPrefix = L"--prefix";
+const wchar_t* CommandArgShort        = L"--short";
+const wchar_t* CommandArgMedium       = L"--medium";
+
+const int CountCharsMedium = 30;
+} // constants
+
+
+namespace
+{
+
+void ShowHelp(const std::wstring& commandName)
+{
+    std::wcout << L"Usage: " << commandName <<L" [" <<
+        constants::CommandArgFromClip << L"][" <<
+        constants::CommandArgToClip << L"][" <<
+        constants::CommandArgBranchPrefix << L" team/developer] <title string>" << std::endl;
+    std::wcout << '\t' << constants::CommandArgFromClip      << L"\tSource string is taken from clipboard. <title string> is optional" << std::endl;
+    std::wcout << '\t' << constants::CommandArgToClip        << L"\tThe result of the conversion to be copied to the clipboard" << std::endl;
+    std::wcout << '\t' << constants::CommandArgBranchPrefix  << L"\t\tTeam and developer name will be added to the beginning of the branch name" << std::endl;
+}
+
+}
+
+int wmain(int argc, wchar_t *argv[], wchar_t *envp[])
+{
+    if (argc <= 1)
+    {
+        ShowHelp(::PathFindFileNameW(argv[0]));
+        return 2;
+    }
+
+    // parse input arguments
+    int position = {};
+    const auto explicitArgs = arg_parser::ExtractExplicitArguments(argc, argv, position);
+    auto inputTitle = arg_parser::ExtractRestArguments(argc, argv, position);
+
+    const bool destinationTitleToClipboard = arg_parser::HasArgument(explicitArgs, constants::CommandArgToClip);
+    const bool shrinkShort = arg_parser::HasArgument(explicitArgs, constants::CommandArgShort);
+    const bool shrinkMedium = arg_parser::HasArgument(explicitArgs, constants::CommandArgMedium);
+    const std::wstring branchPrefix = arg_parser::GetArgumentOrDefault(explicitArgs, constants::CommandArgBranchPrefix);
+    if (arg_parser::HasArgument(explicitArgs, constants::CommandArgBranchPrefix) && branchPrefix.empty())
+    {
+        std::cout << "No prefix specified" << std::endl;
+        return 2;
+    }
+    if (arg_parser::HasArgument(explicitArgs, constants::CommandArgFromClip))
+    {
+        inputTitle = clipboard::GetClipboard();
+    }
+    if (inputTitle.empty())
+    {
+        std::cout << "No title string specified" << std::endl;
+        return 0;
+    }
+
+    // process
+    auto destinationTitle = string_transform::TransformString(inputTitle);
+    if (shrinkShort)
+    {
+        if (string_transform::ShrinkShort(destinationTitle))
+        {
+            std::cout << "Shrinks to short" << std::endl;
+        }
+    }
+    else if (shrinkMedium)
+    {
+        if (string_transform::ShrinkN(destinationTitle, constants::CountCharsMedium))
+        {
+            std::cout << "Shrinks to medium" << std::endl;
+        }
+    }
+    string_transform::RemoveSeparator(destinationTitle);
+    string_transform::ReplaceSubstring(destinationTitle, L"change_request", L"cr");
+
+    if (!branchPrefix.empty())
+    {
+        destinationTitle = branchPrefix + L"/" + destinationTitle;
+    }
+
+    // output result
+    if (destinationTitleToClipboard)
+    {
+        clipboard::SetClipboard(destinationTitle);
+        std::cout << "Copied to clipboard" << std::endl;
+    }
+    else
+    {
+        std::wcout << destinationTitle << std::endl;
+    }
+
+    return 0;
+}
